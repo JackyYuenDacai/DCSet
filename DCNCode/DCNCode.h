f35f601b497a4aa93ac56c690e6aab2adc97fbf7
JackyYuen <562422313@qq.com>
2018-01-25 21:12:07 +0800

    Update DCNCode.h
    add key encryption ( det(key matrix) need to be 1)

--- DCNCode/DCNCode.h
+++ DCNCode/DCNCode.h
@@ -1,16 +1,86 @@
-#include<DCSet/DCNNode.h>
-
+#include"DCNNode.h"
+#include<stdio.h>
 #ifndef DCNCode_H
  #define DCNCode_H
 #endif
 unsigned char HexNo[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 class DCNCode
-{
-public:
+{	
 	DCNStr Data;
 	long long Length;
+	long long encrypt_key = 0;
+	long long decrypt_key = 0;
+public:
+
+#define PUSH_KEYA(a,b,c,d) encrypt_key = a & 0xFF << 24 +  b & 0xFF << 16 + c & 0xFF << 8 + d & 0xFF 
+#define PUSH_KEYB(a,b,c,d) encrypt_key = a & 0xFF << 56 +  b & 0xFF << 48 + c & 0xFF << 40 + d & 0xFF <<32
+
+#define GET_KEYAA ((encrypt_key & 0xFF << 24) >> 24)
+#define GET_KEYAB ((encrypt_key & 0xFF << 16) >> 16)
+#define GET_KEYAC ((encrypt_key & 0xFF << 8) >> 8)
+#define GET_KEYAD (encrypt_key & 0xFF )
+
+#define GET_MOD(a) (a<0?128*(-a/128+1)+a:((int)a)%256)
+
+	void SetEncryptKey(long long key){
+		encrypt_key=key;
+	}
+	void EKeyEmb(){
+		float det_a=GET_KEYAA*GET_KEYAD-GET_KEYAB*GET_KEYAC;
+		if(det_a != 1) return;
+		unsigned char*TurnStr=(unsigned char*)(char*)Data; 
+		int a,b,c,d;
+		Length=Data.GetLength();
+		Data.Insert(0,8);
+		//Data[0]=((unsigned char*)&Length)[0];
+		//Data[1]=((unsigned char*)&Length)[1];
+		//Data[2]=((unsigned char*)&Length)[2];
+		//Data[3]=((unsigned char*)&Length)[3];
+		//Data[Data.GetLength() - Data.GetLength()%4 + 8 - 1]='\0';
+		TurnStr=(unsigned char*)(char*)Data;
+		*((long long int *)TurnStr)=(unsigned long long int)Length;
+		Data.DelAll();
+		Length+=8;
+		// 0 1  4 5    0*4+1*6    0*5+1*7
+		// 2 3  6 7    2*4+3*6    2*5+3*7
+		for(long long int i=0;i<Length;i+=4){
+			a = TurnStr[i + 0];b = TurnStr[i + 1];c = TurnStr[i + 2];d =TurnStr[i + 3];
+			TurnStr[i + 0]=GET_MOD( a * GET_KEYAA +  b * GET_KEYAC );
+			TurnStr[i + 1]=GET_MOD( a * GET_KEYAB +  b * GET_KEYAD );
+			TurnStr[i + 2]=GET_MOD( c * GET_KEYAA +  d * GET_KEYAC );
+			TurnStr[i + 3]=GET_MOD( c * GET_KEYAB +  d * GET_KEYAD );
+		}
+		Data.AddString((char*)TurnStr,Length);
+	}
+	void DKeyEmb(){				
+		float det_a=GET_KEYAA*GET_KEYAD-GET_KEYAB*GET_KEYAC;
+		if(det_a != 1) return;
+		unsigned char*TurnStr=(unsigned char*)(char*)Data; 
+
+		float 	inv_a=	GET_KEYAD/det_a,
+				inv_b=-	GET_KEYAB/det_a,
+				inv_c=-	GET_KEYAC/det_a,
+				inv_d=	GET_KEYAA/det_a;
+		long long int original_length =0;
+		int a,b,c,d;
+		Length=Data.GetLength();
+		//printf("Length : %d \n",Length);
+		Data.DelAll();
+		for(long long int i=0;i<Length;i+=4){
+			a = TurnStr[i + 0];b = TurnStr[i + 1];c = TurnStr[i + 2];d =TurnStr[i + 3];
+			TurnStr[i + 0]=GET_MOD((int)( a * inv_a +  b * inv_c ));
+			TurnStr[i + 1]=GET_MOD((int)( a * inv_b +  b * inv_d ));
+			TurnStr[i + 2]=GET_MOD((int)( c * inv_a +  d * inv_c ));
+			TurnStr[i + 3]=GET_MOD((int)( c * inv_b +  d * inv_d ));
+		}
+		original_length = *((long long int*)TurnStr);//printf("Length : %d \n",original_length);	
+		//for(int i=0;i<original_length;i++)
+		//	 printf("%d ",(char*)(TurnStr+8)[i]);	printf("\n");
+		//original_length = 10;
+		Data.AddString((char*)(TurnStr+8),original_length);
+	}
 	void EBinary(){
-		char*TurnStr=(char*)Data;
+		unsigned char*TurnStr=(unsigned char*)(char*)Data;
 		Length=Data.GetLength();
 		Data.DelAll();
 		for(long long int i=0,En=8;i<Length;i++,En+=18)
@@ -69,13 +139,15 @@ public:
 	void DBinary()
 	{	
 		char*TurnStr=(char*)Data;
+		Length = 0;
+		long long int data_length = Data.GetLength();
 		Data.DelAll();
-		for(int i=0,En=0;TurnStr[i]!=0;i+=9,En++){
+		for(int i=0,En=0;Length<data_length;i+=9,En++){
 			char Value=0;
-			
 			for(int EM=1,C=7;EM<9;EM++)
 				(Value+=(TurnStr[EM+i]-'0')<<C),(C==0?C=0:C--);
 			Data[En]=Value;
+			Length += 1;
 		}
 		/*short*TurnStr=Data.ToShortInt();
 		Data.DelAll();
@@ -154,16 +226,18 @@ public:
 		}
 	}
 	void Encrypt(){
-		EBinary();Data.ClearArrayLog();
+		EBinary();	Data.ClearArrayLog();
 		ENrziCode();Data.ClearArrayLog();
-		EUpper();Data.ClearArrayLog();
-		EFinal();Data.ClearArrayLog();
+		EUpper();	Data.ClearArrayLog();
+		EFinal();	Data.ClearArrayLog();
+		EKeyEmb();Data.ClearArrayLog();
 	}
 	void Decrypt(){
-		DFinal();Data.ClearArrayLog();
-		DUpper();Data.ClearArrayLog();
+		DKeyEmb();Data.ClearArrayLog();
+		DFinal();	Data.ClearArrayLog();
+		DUpper();	Data.ClearArrayLog();
 		DNrziCode();Data.ClearArrayLog();
-		DBinary();Data.ClearArrayLog();		
+		DBinary();	Data.ClearArrayLog();		
 	}
 	void AddString(char*data,long long len){
 		Data.AddString(data,len);
@@ -171,8 +245,21 @@ public:
 	void operator=(char*array){
 		Data=array;
 	}
+ 
 	void operator=(const char*array){
 		Data=array;
+	}
+	unsigned char& operator [](long long index){
+		return (Data.NodeSet(index)->c);
+	}
+	unsigned char& operator [](long index){
+		return (Data.NodeSet(index)->c);
+	}	
+	unsigned char& operator [](int index){
+		return (Data.NodeSet(index)->c);
+	}
+	unsigned char& operator [](short index){
+		return (Data.NodeSet(index)->c);
 	}
 	long long GetLength(){
 		return Data.GetLength();
@@ -185,4 +272,3 @@ public:
 		return Data;
 	}
 };
-
